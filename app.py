5689b958e79739ca48fa0e30fd5b8f3d9c7ef2ae
Atif20024 <atifhsn16@gmail.com>
2022-11-29 22:32:43 +0530

    hosted with ngrok

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, app, jsonify, render_template
-
 from google_drive_downloader import GoogleDriveDownloader as gdd
 import whisper
+import pandas as pd
 from moviepy.editor import *
 
 app = Flask(__name__)
@@ -17,8 +17,9 @@ def transcribe():
     # data = request.json['data']
     # file_id = data['file_id']
     file_id = request.form.values()
+    print(file_id)
     video_file = gdd.download_file_from_google_drive(file_id=file_id,
-        dest_path='./data/video.mp4', unzip=False)
+        dest_path='./data/video.mp4', unzip=False, overwrite=True)
     
     # tiny, base, small, medium, large: pick one model among these
     model = whisper.load_model("base")
@@ -30,11 +31,24 @@ def transcribe():
     
     # you can print this so that there is no need to upload the file.
     transcript = output['text']
-    print(transcript)
-    with open('./data/final_transcript.txt', 'w') as f:
-        f.write(transcript)
-    return render_template('main_page.html', prediction_text="{}".format(transcript))
+    transcription_df = pd.json_normalize(output['segments'])
+
+    transcription_df['temp'] = "{"+ transcription_df['start'].astype(str) + ", " + transcription_df['end'].astype(str) +"}" + transcription_df['text'] 
+
+    transcription1 = '\n'.join(transcription_df['temp'].to_list())
+
+    transcription2 = "\n".join(transcription_df['text'])
+
+    print(transcription2)
+    with open('./data/final_transcript1.txt', 'w') as f:
+        f.write(transcription1)
+    with open('./data/final_transcript2.txt', 'w') as f:
+        f.write(transcription2)
+    return render_template('main_page.html', prediction_text="{}".format(transcription2))
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=80)
+## problems facing:
+## 1. ngrok only running when connected to hotspot not with wifi
+## 2. files either self update or file should get deleted once the transcription is ready.
